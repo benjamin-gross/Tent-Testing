22ab1bbcafc98f71b075b9ac05d25fc750832b33
Ben <benarang@gmail.com>
2022-07-01 12:43:07 -0400

    auto updates

--- start.sh
+++ start.sh
@@ -6,10 +6,12 @@ filename="Tent-Testing"
 if [ -d "$filename" ]; then
 echo "Starting Testing"
 cd Tent-Testing
+git pull
 python3 testing.py
 else 
 echo "begining download"
 pip3 install openpyxl
+pip3 install requests 
 
 cd ~
 cd Desktop/
